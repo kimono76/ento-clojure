41ed8184a6dd2cf91419a48160baf40011833797
virgilius-think-wsl <alberto.gruning.zen@gmail.com>
2022-11-21 15:54:14 -0300

    feat: description for project.clj

--- ento-clojure/project.clj
+++ ento-clojure/project.clj
@@ -1,5 +1,5 @@
 (defproject ento-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Math, Statistics, and ML API for Ento project, written in clojure"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
